cb6ffed3daeb2dd37fc3a2edbdec0682a2919563
Tom Dodson <t3dodson@gmail.com>
2025-12-05 23:48:24 -0800

    fix dbus

--- home.nix
+++ home.nix
@@ -1,8 +1,12 @@
-{ identity, stylix, pkgs, ... }: {
+{ identity, pkgs, ... }: {
   home = { inherit (identity) username homeDirectory; };
   imports = [ ./modules ];
   home.preferXdgDirectories = true;
   home.stateVersion = "25.11";
+
+  # Not sure how dconf got here. I think it may be stylix adding themees to gtk etc...
+  dconf.enable = false;
+
   stylix.enableReleaseChecks = false;
   stylix.enable = true;
   stylix.base16Scheme =
