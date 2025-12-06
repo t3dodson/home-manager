5a9228726e96981bb9fabc4606d7c5596eaad54c
Tom Dodson <t3dodson@gmail.com>
2025-12-06 13:07:38 -0800

    prompt

--- modules/shell/prompt.nix
+++ modules/shell/prompt.nix
@@ -3,6 +3,21 @@
   programs.starship = {
     enable = true;
     configPath = "${config.xdg.configHome}/starship/starship.toml";
-    settings = { };
+    settings = {
+      "$schema" = "https://starship.rs/config-schema.json";
+      format = "$time$all$character";
+      directory = let normal_style = "cyan"; in{
+        truncation_length = 8;
+        truncate_to_repo = false;
+        style = normal_style;
+        repo_root_style = normal_style;
+        before_repo_root_style = "brown";
+
+      };
+      time = {
+        disabled = false;
+        format = "[$time](brown) ";
+      };
+    };
   };
 }
